43e844dd9c95a116a1c6b3797f4a9f959b747963
KauanPinheiro <kauan.pnascimento750@gmail.com>
2024-06-13 11:22:18 -0300

    preparando o projeto

---
+++ src/screens/CadastroScreen.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { SafeAreaView } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import CommonStyles from "../CommonStyles";
+import getImgBackground from "../../assets/img/CadastroBackground.png"
+
+export default class CadastroScreen extends Component {
+
+    render() {
+
+        return(
+            <SafeAreaView>
+
+            </SafeAreaView>
+        )
+    }
+}
